1668750ab8a6be44127a807931fcec05c5988d5c
Sebastianthomas94 <Sebastianthomas94@outlook.com>
2019-08-22 14:35:17 +0530

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { DataComponent } from './data/data.component';
 import { ViewComponent } from './view/view.component';
 import { CartComponent } from './cart/cart.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataComponent,
     ViewComponent,
-    CartComponent
+    CartComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
