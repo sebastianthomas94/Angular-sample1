d53d830708a296bfddc287d4672be3d32d1804bd
Sebastianthomas94 <Sebastianthomas94@outlook.com>
2019-08-22 15:44:47 +0530

    calculator

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -9,9 +9,18 @@ export class CalcComponent implements OnInit {
 
   constructor() { }
 
-  add:Boolean=false;
-  sub:Boolean=false;
+  add1:Boolean=false;
+  sub1:Boolean=false;
   ngOnInit() {
+  }
+
+  public add(a,b)
+  {
+    return(a+b);
+  }
+  public sub(a,b)
+  {
+    return(a-b);
   }
 
 }
